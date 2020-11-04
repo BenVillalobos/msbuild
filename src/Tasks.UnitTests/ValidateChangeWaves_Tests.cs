27847ad240c0531893559984f9ed86dda1861130
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2020-11-03 18:32:12 -0800

    Created ValidateChangeWaves task

---
+++ src/Tasks.UnitTests/ValidateChangeWaves_Tests.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Microsoft.Build.Tasks.UnitTests
+{
+    class ValidateChangeWaves_Tests
+    {
+        private readonly ITestOutputHelper _testOutput;
+        public ValidateChangeWaves_Tests(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+        }
+    }
+}
---
+++ src/Tasks/ValidateChangeWaves.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Utilities;
+using System;
+using System.Linq;
+
+namespace Microsoft.Build.Tasks
+{
+    class ValidateChangeWaves : TaskExtension
+    {
+        public override bool Execute()
+        {
+            ChangeWaves.ApplyChangeWave();
+
+            switch (ChangeWaves.ConversionState)
+            {
+                case ChangeWaveConversionState.InvalidFormat:
+                    Log.LogWarningFromResources("ChangeWave_InvalidFormat", Traits.Instance.MSBuildDisableFeaturesFromVersion, $"[{String.Join(", ", ChangeWaves.AllWaves.Select(x => x.ToString()))}]");
+                    break;
+                case ChangeWaveConversionState.OutOfRotation:
+                    Log.LogWarningFromResources("ChangeWave_OutOfRotation", Traits.Instance.MSBuildDisableFeaturesFromVersion, $"[{String.Join(", ", ChangeWaves.AllWaves.Select(x => x.ToString()))}]");
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
