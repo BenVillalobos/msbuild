5618ff27f335f4493cac060e87a68569f8e3d0d6
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2020-11-05 09:13:12 -0800

    Move resource strings. Add task & usingtask

---
+++ src/Tasks.UnitTests/ValidateChangeWaves_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Build.Tasks.UnitTests
+{
+    public class ValidateChangeWaves_Tests
+    {
+        
+    }
+}
---
+++ src/Tasks/ValidateChangeWaves.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Utilities;
+using System;
+using System.Linq;
+
+using MSBuildChangeWaves;
+
+namespace Microsoft.Build.Tasks
+{
+    class ValidateChangeWaves : TaskExtension
+    {
+        public override bool Execute()
+        {
+            ChangeWaves.ApplyChangeWave();
+
+            switch (ChangeWaves.ConversionState)
+            {
+                case ChangeWaves.ChangeWaveConversionState.InvalidFormat:
+                    Log.LogWarningFromResources("ChangeWave_InvalidFormat", Traits.Instance.MSBuildDisableFeaturesFromVersion, $"[{String.Join(", ", ChangeWaves.AllWaves.Select(x => x.ToString()))}]");
+                    break;
+                case ChangeWaves.ChangeWaveConversionState.OutOfRotation:
+                    Log.LogWarningFromResources("ChangeWave_OutOfRotation", Traits.Instance.MSBuildDisableFeaturesFromVersion, $"[{String.Join(", ", ChangeWaves.AllWaves.Select(x => x.ToString()))}]");
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
